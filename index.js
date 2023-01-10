405099f7ab8ce4fecca2f42c4979b822b3856ee6
SirPudwhip <spamfolder3399@gmail.com>
2023-01-10 18:13:24 -0500

    commit tree eeb076489ddb4d4efeea27e0706cb10d2b8dfaaa

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bowl!`);
+    }
+return gifts
+}
+
+wrapGifts(gifts);
+
+function writeCards(name, event) {
+    const messages = []
+        for (let i = 0; i < name.length; i++) {
+            messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        }
+    return messages
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(y) {
+    while (y >= 0) {
+        console.log(y--)
+    }
+}
+
+countDown(10);
